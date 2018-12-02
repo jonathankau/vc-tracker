7443e5f7336b61e9457f8913ea1d6f5926e10c9f
Jonathan Kau <jkau15@gmail.com>
2018-12-01 22:14:51 -0800

    Use ISO8601DateTime for next_follow_up_at.

--- app/graphql/types/investor_target_type.rb
+++ app/graphql/types/investor_target_type.rb
@@ -3,7 +3,7 @@ module Types
     field :id, ID, null: false
 
     field :fundraising_stage, FundraisingStage, null: false
-    field :next_follow_up_at, String, null: true
+    field :next_follow_up_at, GraphQL::Types::ISO8601DateTime, null: true
 
     field :investor, InvestorType, null: false
   end
