0e75d70e576ad265c70be45f00957a9cebecf636
Jonathan Kau <jkau15@gmail.com>
2018-12-03 15:26:33 -0800

    Fix partner meetings stage.

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,7 +2,7 @@ export const FUNDRAISING_STAGE_LABELS = Object.freeze({
   'RESEARCHING': 'Researching',
   'CONTACTED': 'Contacted',
   'INITIAL_MEETING': 'Initial meeting',
-  'PARTNER_MEETING': 'Partner meeting',
+  'PARTNER_MEETINGS': 'Partner meetings',
   'RECEIVED_TERM_SHEET': 'Received term sheet',
   'DUE_DILIGENCE': 'Due diligence',
   'CLOSED_INVESTMENT': 'Closed investment',
