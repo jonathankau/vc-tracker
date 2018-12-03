10d5bdd8ecb1870e00c699d009cf56bbeaa8b10a
Jonathan Kau <jkau15@gmail.com>
2018-12-02 22:13:38 -0800

    Implement initial investor tracker list UI.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,18 +3,43 @@ import gql from 'graphql-tag';
 import React from 'react';
 import { ApolloProvider, Query } from 'react-apollo';
 
-import '../styles/App.css';
+import {
+  Container,
+  Heading,
+  Box,
+  Content,
+  Media,
+  Image,
+  Progress,
+  Columns
+} from 'react-bulma-components/full';
+
+import '../styles/App.scss';
 
 const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql'
 });
 
-const SampleQueryComponent = () => (
+const AllInvestorTargetsQueryComponent = () => (
   <Query
     query={gql`
       {
         investorTargets {
           id
+          fundraisingStage
+          nextFollowUpAt
+
+          investor {
+            person {
+              fullName
+              email
+              signalProfileUrl
+            }
+
+            firm {
+              name
+            }
+          }
         }
       }
     `}
@@ -23,15 +48,53 @@ const SampleQueryComponent = () => (
       if (loading) return <p>Loading</p>;
       if (error) return <p>Error</p>;
 
-      console.log(data);
-      return <p>Done</p>;
+      return data.investorTargets.map(({
+        id,
+        fundraisingStage,
+        nextFollowUpAt,
+        investor
+      }) => (
+        <Box>
+          <Media>
+            <Media.Item renderAs="figure" position="left">
+              <Image renderAs="p" size={64} alt="64x64" src="http://bulma.io/images/placeholders/128x128.png" />
+            </Media.Item>
+
+            <Media.Item>
+              <Columns>
+                <Columns.Column>
+                  <Content>
+                    <strong>{investor.person.fullName}</strong>
+                    <p>
+                      { investor.firm ? investor.firm.name : 'Individual/Angel' }<br />
+                      { investor.person.email ? <span>{investor.person.email}<br /></span> : null }
+                    </p>
+                  </Content>
+                </Columns.Column>
+
+                <Columns.Column>
+                  <div className="is-pulled-right">
+                    {fundraisingStage}
+                    <Progress style={{ width: 200 }} max={100} value={15} color="warning"/>
+                  </div>
+                </Columns.Column>
+              </Columns>
+
+            </Media.Item>
+          </Media>
+        </Box>
+      ));
     }}
   </Query>
 );
 
 const App = () => (
   <ApolloProvider client={client}>
-    <SampleQueryComponent />
+    <Container id="app-container" className="has-text-centered">
+      <Heading>Investor Tracker</Heading>
+
+      <AllInvestorTargetsQueryComponent />
+    </Container>
   </ApolloProvider>
 );
 
